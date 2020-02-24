5416b17e4983e8a6b302a4abddfcc995c19a9cf8
indiegameband <indiegameband@gmail.com>
2020-02-24 23:54:34 +0300

    Add project files.

---
+++ HW13.cpp
@@ -0,0 +1,11 @@
+// HW13.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+    int result = sum(4, 7);
+    std::cout << "result = "<<result;
+}
